3a9dbce94d436810fcadb29c3e6031c15cb9373e
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-08-17 20:49:19 +0900

    init commit

---
+++ src/main/java/com/githubsub/GithubsubApplication.java
@@ -0,0 +1,13 @@
+package com.githubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubsubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/githubsub/GithubsubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubsubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
